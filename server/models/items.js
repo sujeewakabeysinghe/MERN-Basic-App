80c5ee0ae4d4cc0f04a322c627a1e53a8a3f4ba7
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-12-30 12:59:06 +0530

    item model

--- server/models/items.js
+++ server/models/items.js
@@ -2,7 +2,9 @@ const Mongoose=require("mongoose");
 const Schema=Mongoose.Schema;
 
 
-const Items=new Schema({
+const Item=new Schema({
     name:{type:String,required:true},
     date:{type:Date,default:Date.now},
 });
+
+const Items=module.exports=Mongoose.model("item",Item);
