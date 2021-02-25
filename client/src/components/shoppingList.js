472fc87507cd47459f62249c27df3fb1b2835c9a
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2021-02-25 21:58:46 +0530

    shopping liat component

---
+++ client/src/components/shoppingList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class shoppingList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default shoppingList
